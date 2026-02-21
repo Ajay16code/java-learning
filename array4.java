8179c99fb953897c1a0c6b3053feae089a958932
Ajay S <147607241+Ajay16code@users.noreply.github.com>
2026-02-22 00:03:51 +0530

    Update array4.java

--- array4.java
+++ array4.java
@@ -4,6 +4,9 @@ public class array4
 {
    public static void main (String [] args)
 {
-      System.out.println("Array4");
+      int [] arr = new int[]{1,2,3,4,5};
+      for(int i=0; i<arr.length;i++){
+      System.out.println(arr+" ");
+      }
 }
 }
