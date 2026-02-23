85772d594f8a8656d482a10323ae0013a3e4c772
Ajay S <147607241+Ajay16code@users.noreply.github.com>
2026-02-23 18:59:36 +0530

    array initialization to use user input
    Modified the array initialization to take user input for the array size and elements.

--- array4.java
+++ array4.java
@@ -1,12 +1,16 @@
 import java.util.*;
-
+import java.util.Scanner;
 public class array4
 {
    public static void main (String [] args)
-{
-      int [] arr = new int[]{1,2,3,4,5};
+{   
+      int n = sc.nextInt();
+      int [] arr = new int[n];
       for(int i=0; i<arr.length;i++){
-      System.out.println(arr+" ");
+          arr[i] = sc.nextInt();
+      }
+   for(int i=0; i<arr.length;i++){
+          System.out.print(arr[i]+" ");
       }
 }
 }
