01e847f5f0faf5c1a4512d4ce3096d7dc12ce490
Ajay S <147607241+Ajay16code@users.noreply.github.com>
2026-02-26 07:53:40 +0530

    threading.java

---
+++ threading.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class threading{
+     public static void main(String [] args)
+{
+       System.out.println("Making bake :"threading.currentthread.getId())
+       int count = 0;
+       threading c1 = new threading();
+       c1 = count++;
+       System.out.print(c1);
+}
+
+}
