fff6d77781c42bd5739526a7da8b4f378ba79da9
Ajay S <147607241+Ajay16code@users.noreply.github.com>
2026-02-27 18:03:10 +0530

    Rename class and add main method
    Renamed class from 'File' to 'file1' and added main method.

---
+++ file1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+import java.io.file;
+
+public class file1
+  {
+    public static void main(String[]args)
+    {
+      
+      FileReader f1 = new FileReader("Choose a path");
+      Scanner file = new Scanner(f1);
+    }
+  }
