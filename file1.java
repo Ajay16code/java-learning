48074676fc8a3e32413bfced220d9ed802e88b7b
Ajay S <147607241+Ajay16code@users.noreply.github.com>
2026-02-28 18:31:56 +0530

    Refactor file1 and add file2 class writer and reader
    Refactor scanner variable names and add file2 class with main method.

--- file1.java
+++ file1.java
@@ -8,6 +8,19 @@ public class file1
     {
       
       FileReader f1 = new FileReader("Choose a path");
-      Scanner file = new Scanner(f1);
+      Scanner fil1 = new Scanner(f1);
+    }
+  }
+
+public class file2 extends file1
+  {
+    public static void main(String[] args)
+    {
+      FileWriter f2 = new FileWriter("Choose a path");
+      Scanner fil2 = new Scanner(f2);
+      while(String fil2 != fil2.nextLine())
+        {
+        System.out.println(" ");
+        }
     }
   }
