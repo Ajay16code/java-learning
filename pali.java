63e411f6957966d5cfdb7d9e4e333c8feaf32c2c
Ajay S <147607241+Ajay16code@users.noreply.github.com>
2026-03-12 22:30:09 +0530

    Create pali.java

---
+++ pali.java
@@ -0,0 +1,27 @@
+public class PalindromeCheck {
+
+    public static boolean isPalindrome(String str) {
+        str = str.toLowerCase();
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String word = "madam";
+
+        if (isPalindrome(word)) {
+            System.out.println(word + " is a palindrome");
+        } else {
+            System.out.println(word + " is not a palindrome");
+        }
+    }
+}
