760d19d6e6c533783fdeb6e92e640dcb0db208b4
Ajay S <147607241+Ajay16code@users.noreply.github.com>
2026-03-15 09:34:43 +0530

    Refactor Main class to use user input for calculations
    Updated the program to take user input for two integers and perform arithmetic operations.

--- Add.java
+++ Add.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub!");
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println("Hello");
         
-        int a = 10;
-        int b = 20;
-        int sum = a + b;
+        
+        int sum = n + m;
+        int sub = n-m;
+        int mul = n*m;
+        int div = n/m;
+        int rem = n%m;
 
         System.out.println("Sum = " + sum);
+        
     }
 }
