73be3edde73b923568e524134ed1a055af79cb09
Ajay S <147607241+Ajay16code@users.noreply.github.com>
2026-05-31 20:14:19 +0530

    Create destroying asteriods.java

---
+++ destroying asteriods.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        
+        long currMass = mass;
+        
+        for (int a : asteroids) {
+            if (currMass < a) {
+                return false;
+            }
+            currMass += a;
+        }
+        
+        return true;
+    }
+}
