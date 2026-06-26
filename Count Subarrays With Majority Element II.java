5c79d55ac89d837b3ade19981c20a6b582c5b929
Ajay S <147607241+Ajay16code@users.noreply.github.com>
2026-06-26 20:31:30 +0530

    Create Count Subarrays With Majority Element II.java

---
+++ Count Subarrays With Majority Element II.java
@@ -0,0 +1,55 @@
+class Solution {
+
+    class Fenwick {
+        int[] bit;
+        int n;
+
+        Fenwick(int size) {
+            n = size;
+            bit = new int[n + 1];
+        }
+
+        void update(int idx, int val) {
+            while (idx <= n) {
+                bit[idx] += val;
+                idx += idx & -idx;
+            }
+        }
+
+        int query(int idx) {
+            int sum = 0;
+            while (idx > 0) {
+                sum += bit[idx];
+                idx -= idx & -idx;
+            }
+            return sum;
+        }
+    }
+
+    public long countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+
+        int offset = n + 2;
+        Fenwick ft = new Fenwick(2 * n + 5);
+
+        long ans = 0;
+        int prefix = 0;
+
+        // Initial prefix sum = 0
+        ft.update(offset, 1);
+
+        for (int x : nums) {
+            if (x == target)
+                prefix++;
+            else
+                prefix--;
+
+            // Count previous prefix sums < current prefix
+            ans += ft.query(prefix + offset - 1);
+
+            ft.update(prefix + offset, 1);
+        }
+
+        return ans;
+    }
+}
